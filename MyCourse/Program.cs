c921f5a635d52a1fa33eed164b01e97c22bf18ce
amargutti <a.margutti@doctorshop.it>
2025-11-06 18:04:52 +0100

    add comment on middleware

--- MyCourse/Program.cs
+++ MyCourse/Program.cs
@@ -15,7 +15,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseStaticFiles();
+app.UseStaticFiles(); //Middleware per recuperare i file nella cartella wwwroot (es. immagini)
 
 app.UseRouting();
 
