67f89d3b38b9ccb5762e5d4cd5b66702b75e397c
amargutti <a.margutti@doctorshop.it>
2025-11-07 10:17:49 +0100

    added course controller with index and details action

---
+++ MyCourse/Controllers/Courses/CoursesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCourse.Controllers.Courses
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Sono la pagina lista di Corsi");
+        }
+
+        public IActionResult Detail(string id) {
+            return Content($"Sarò la view di dettaglio con questo {id}");
+        }
+    }
+}
