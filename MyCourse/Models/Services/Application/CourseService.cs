be9b42bf80c06153bbe0b7633a88ec6da8b41183
AndreaMargutti <marguttiandrea30@gmail.com>
2025-11-08 18:03:26 +0100

    better implementation of Rating

--- MyCourse/Models/Services/Application/CourseService.cs
+++ MyCourse/Models/Services/Application/CourseService.cs
@@ -10,17 +10,21 @@ namespace MyCourse.Models.Services.Application
         public List<CourseViewModel> GetCourses()
         {
             var rand = new Random();
+            float rating ()
+            {
+                return (float)(rand.NextDouble() * (4 + 1));
+            }
             List<CourseViewModel> courses = new List<CourseViewModel>();
 
             for (int i = 0; i <= 20; i++)
             {
                 var course = new CourseViewModel
                 {
-                    Id = i,
-                    Title = $"Corso {i}",
+                    Id = i + 1,
+                    Title = $"Corso {i + 1}",
                     Author = "Andrea Margutti",
                     ImagePath = "/logo.svg",
-                    Rating = rand.Next(5),
+                    Rating = rating(),
                     FullPrice = new ValueObjects.Money { Amount = 19.99m, Currency = Enums.Currency.EUR },
                     CurrentPrice = new ValueObjects.Money { Amount = 15.99m, Currency = Enums.Currency.EUR }
                 };
