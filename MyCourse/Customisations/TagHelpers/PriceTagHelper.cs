b8cc064fb330861338c6303f76ed0177778a2f21
amargutti <a.margutti@doctorshop.it>
2025-11-17 15:34:51 +0100

    add price tag helper

---
+++ MyCourse/Customisations/TagHelpers/PriceTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using MyCourse.Models.ValueObjects;
+
+namespace MyCourse.Customisations.TagHelpers
+{
+    public class PriceTagHelper : TagHelper
+    {
+        public Money FullPrice { get; set; }
+        public Money CurrenPrice { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.AppendHtml($"{CurrenPrice}");
+
+            if(!CurrenPrice.Equals(FullPrice))
+            {
+                output.Content.AppendHtml($"<br /> <s class=\"text-secondary\">{FullPrice}</s>");
+            }
+        }
+    }
+}
