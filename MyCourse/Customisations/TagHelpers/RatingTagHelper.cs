fc45955e62f513e992a3b4860eb166bbaad9ea48
amargutti <a.margutti@doctorshop.it>
2025-11-17 15:25:27 +0100

    add rating tag helper

---
+++ MyCourse/Customisations/TagHelpers/RatingTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MyCourse.Customisations.TagHelpers
+{
+    public class RatingTagHelper : TagHelper
+    {
+        public double Value { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                if (Value >= i)
+                {
+                    output.Content.AppendHtml("<i class=\"bi bi-star-fill text-warning\"></i>");
+                }
+                else if (Value > i - 1)
+                {
+                    output.Content.AppendHtml("<i class=\"bi bi-star-half text-warning\"></i>");
+                }
+                else
+                {
+                    output.Content.AppendHtml("<i class=\"bi bi-star\"></i>");
+                }
+            }
+        }
+    }
+}
